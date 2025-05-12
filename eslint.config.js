2d07fed4810585be4375c1e41d263120dcab1158
jackyou <jjjj88521@gmail.com>
2025-05-12 11:51:29 +0800

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+
+  {
+    vue: true,
+    ignores: ['**/__registry__/index.ts', '**/__registry__/block.ts'],
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/one-component-per-file': 0,
+      'vue/no-reserved-component-names': 0,
+      'vue/no-useless-v-bind': 0,
+    },
+  },
+  {
+    files: [
+      '**/registry/default/examples/*.vue',
+      '**/registry/new-york/examples/*.vue',
+      '**/registry/new-york-v4/examples/*.vue',
+    ],
+    rules: {
+      'no-alert': 0,
+      'no-console': 0,
+    },
+  },
+  {
+    // Without `files`, they are general rules for all files
+    rules: {
+      'symbol-description': 0,
+      'no-console': 1,
+      'no-tabs': 0,
+      'import/first': 0,
+      'node/prefer-global/process': 0,
+      'style/no-tabs': 0,
+      'unused-imports/no-unused-vars': 0,
+      'unicorn/no-new-array': 0,
+      'import-x/consistent-type-specifier-style': 0,
+    },
+  },
+)
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './src/components/ui',
+  },
+})
